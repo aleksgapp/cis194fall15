c4b5f4eb6e5fb714b65e13f446b3cd2104e71af8
Aleksey Gaponov <aleks.gapp@gmail.com>
2016-06-09 00:09:12 +0200

    Homework 5, exercise 2.

--- hw05/hw05.hs
+++ hw05/hw05.hs
@@ -1,8 +1,9 @@
 module Hw05 where
 
-import Data.ByteString (ByteString)
-import Data.List
-import qualified Data.ByteString as BS
+import Data.ByteString.Lazy (ByteString)
+import Data.Bits (xor)
+
+import qualified Data.ByteString.Lazy as BS
 
 -- Exercise 1
 
@@ -10,11 +11,19 @@ getSecret :: FilePath -> FilePath -> IO ByteString
 getSecret oldf newf = do
     old <- BS.readFile oldf
     new <- BS.readFile newf
-    return $ BS.pack $ BS.unpack old \\ BS.unpack new
+    return $ BS.pack $ filter (/= 0) $ BS.zipWith xor old new
 
 exercise1 :: IO ByteString
-exercise1 = getSecret "clues/dog.jpg" "clues/dog-original.jpg"
+exercise1 = getSecret "clues/dog-original.jpg" "clues/dog.jpg"
 
 -- Exercise 2
 
--- decryptWithKey :: ByteString -> FilePath -> IO ()
+decryptWithKey :: ByteString -> FilePath -> IO ()
+decryptWithKey key path = do
+    file <- BS.readFile (path ++ ".enc")
+    BS.writeFile path (BS.pack $ BS.zipWith xor (BS.cycle key) file)
+
+exercise2 :: IO ()
+exercise2 = do
+    key <- exercise1
+    decryptWithKey key "clues/victims.json"
